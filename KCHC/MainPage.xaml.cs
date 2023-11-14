2fa3230b4e1eb88fba6b60a46df1b9b3de2eae07
George Smyrlis <smirlagouras@gmail.com>
2023-11-14 22:42:19 +0200

    Add project files.

---
+++ KCHC/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using KCHC.Models;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KCHC
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Artist> Artists { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            // Initialize and set the Artists collection with your data
+            Artists = new ObservableCollection<Artist>
+            {
+                new Artist { PhotoPath = "SOR.jpg", Name = "Swarm Of Rats", Description = "Katerinian HC band" },
+                new Artist { PhotoPath = "SAD.jpg", Name = "S.A.D.", Description = "A creation of destruction and pain" },
+                new Artist { PhotoPath = "Libys.jpg", Name = "Libys", Description = "Katerinian Indie Star" },
+                // Add more artists as needed
+            };
+
+            BindingContext = Artists;
+        }
+    }
+}
---
+++ KCHC/Models/Artist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KCHC.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; } = string.Empty;
+        public string PhotoPath { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ContentImage { get; set; } = string.Empty;
+        public string SpotifyAccountUrl { get; set; } = string.Empty;
+        public string YoutubeAccountUrl { get; set; } = string.Empty;
+        public string BandCampAccountUrl { get; set; } = string.Empty;
+        public string ExtraDescription { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Not full Constructor. This constructor is only for first page
+        /// </summary>
+        /// <param name="name"> The Name of the Artist</param>
+        /// <param name="photoPath">The photopath of the artist inside this solution</param>
+        /// <param name="description">General description about the artist</param>
+        public Artist(string name, string photoPath, string description)
+        {
+            Name = name;
+            PhotoPath = photoPath;
+            Description = description;
+        }
+
+        /// <summary>
+        /// Empty Constructor
+        /// </summary>
+        public Artist()
+        {
+        }
+    }
+}
