b7d47ddcc28156d3ed4f6709905757b3a6566863
George Smyrlis <gsmyrlof@yahoo.com>
2023-11-28 08:36:19 +0200

    added class Band, added ArtistPage, added socialmedia photos

---
+++ KCHC/ArtistPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System;
+
+namespace KCHC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ArtistPage : ContentPage
+    {
+        public ArtistPage()
+        {
+            InitializeComponent();
+        }
+        public ArtistPage(Models.Artist artist)
+        {
+            InitializeComponent();
+            BindingContext = artist;
+            ImageArtist.Source = artist.PhotoPath;
+            if (artist.DateTime != null && artist.DateTime != System.DateTime.MinValue)
+            {
+                // Create a new Label for displaying "Created on: "
+                Label createdOnLabel = new Label
+                {
+                    Text = "Created on: " + artist.DateTime.ToString("dd MMM yyyy"),
+                    FontSize = 18,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand
+                };
+                GridForData.Children.Add(createdOnLabel);
+            }
+
+            int count = 0;
+            if (!string.IsNullOrEmpty(artist.BandCampAccountUrl))
+            {
+                Button buttonBandCamp = CreateSocialButton("bandcamp.jpg", artist.BandCampAccountUrl);
+                GridForConnections.Children.Add(buttonBandCamp, count++, 0);
+            }
+
+            if (!string.IsNullOrEmpty(artist.SpotifyAccountUrl))
+            {
+                Button buttonSpotify = CreateSocialButton("spotify.jpg", artist.SpotifyAccountUrl);
+                GridForConnections.Children.Add(buttonSpotify, count++, 0);
+            }
+
+            if (!string.IsNullOrEmpty(artist.YoutubeAccountUrl))
+            {
+                Button buttonYouTube = CreateSocialButton("youtube.jpg", artist.YoutubeAccountUrl);
+                GridForConnections.Children.Add(buttonYouTube, count++, 0);
+            }
+        }
+        // Helper method to create a social media button with an image
+        private Button CreateSocialButton(string imageName, string url)
+        {
+            return new Button
+            {
+                HeightRequest = 40, // Set the height of the button
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.End,
+                ImageSource = imageName,
+                Command = new Command(async () =>
+                {
+                    // Handle button click (e.g., open the URL)
+                    if (!string.IsNullOrEmpty(url))
+                    {
+                        await Launcher.OpenAsync(new Uri(url));
+                    }
+                })
+            };
+        }
+
+
+    }
+}
--- KCHC/MainPage.xaml.cs
+++ KCHC/MainPage.xaml.cs
@@ -20,16 +20,17 @@ namespace KCHC
             // Initialize and set the Artists collection with your data
             Artists = new ObservableCollection<Artist>
             {
-                new Artist {PhotoPath = "Akatalogistoi.jpg", Name="Akatalogistoi", Description = "Katerinian Hardcopunk"},
-                new Artist {PhotoPath = "Killathea.jpg", Name="Killathea", Description = "RnB"},
-                new Artist { PhotoPath = "Libys.jpg", Name = "Libys", Description = "Indie" },
-                new Artist {PhotoPath = "Mariospol.jpg", Name="Marios Pol", Description = "DJ and Laika"},
-                new Artist { PhotoPath = "SAD.jpg", Name = "S.A.D.", Description = "Folk country Deathcore" },
-                new Artist { PhotoPath = "SOR.jpg", Name = "Swarm Of Rats", Description = "Hardcore" },
-                new Artist {PhotoPath = "Spyridwn.JPG", Name = "Spyridon", Description = "Hard Indie Metal"},
-                new Artist { PhotoPath = "TaratsaParadeisou.jpg", Name = "Taratsa Paradeisou", Description = "Blues Indie"}
+                new Artist { PhotoPath = "Akatalogistoi.jpg", Name="Akatalogistoi", Description = "Katerinian Hardcopunk", IsABand = true},
+                new Artist { PhotoPath = "Killathea.jpg", Name="Killathea", Description = "RnB"},
+                new Artist { PhotoPath = "Libys.jpg", Name = "Libys", Description = "Indie", ExtraDescription="Μόλις κυκλοφόρησε το δεύτερο άλμπουμ μου ✨ Ένα mixtape που ηχογραφήθηκε αυθόρμητα μέσα σε λιγότερο από έναν μήνα. Το κρατούσα σχεδόν δύο χρόνια από την δημιουργία του. Ποτέ δεν μπορούσα να αποφασίσω με σιγουριά αν μου άρεσε ή όχι, αλλά σίγουρα μπορώ να πω ότι περιγράφει καλά μια περίοδο της ζωής μου.", SpotifyAccountUrl="https://open.spotify.com/artist/7FUxLkwKbV3eoAgLUkzDXo?si=1Ufz3HGHQQaRPJlBr3W6ww&nd=1&dlsi=b53f102b24924a5f", YoutubeAccountUrl="https://www.youtube.com/@Libys", BandCampAccountUrl="https://libys.bandcamp.com/track/ifeellibys-5" },
+                new Artist { PhotoPath = "Mariospol.jpg", Name="Marios Pol", Description = "DJ and Laika"},
+                new Artist { PhotoPath = "SAD.jpg", Name = "S.A.D.", Description = "Folk country Deathcore", IsABand = true },
+                new Artist { PhotoPath = "SOR.jpg", Name = "Swarm Of Rats", Description = "Hardcore", IsABand = true },
+                new Artist { PhotoPath = "Spyridwn.JPG", Name = "Spyridon", Description = "Hard Indie Metal"},
+                new Artist { PhotoPath = "TaratsaParadeisou.jpg", Name = "Taratsa Paradeisou", Description = "Blues Indie", IsABand = true}
             };
             BindingContext = this;
+
         }
 
         private async void OnKinimatoramaImageClicked(object sender, EventArgs e)
@@ -37,6 +38,14 @@ namespace KCHC
             if (Application.Current.MainPage?.Navigation != null)
             {
                 await Application.Current.MainPage.Navigation.PushAsync(new KiminatoramaPage());
+            }
+        }
+
+        private async void OnImageTapped(object sender, EventArgs e)
+        {
+            if (sender is Image tappedImage && tappedImage.BindingContext is Models.Artist selectedArtist)
+            {
+                await Navigation.PushAsync(new ArtistPage(selectedArtist));
             }
         }
     }
